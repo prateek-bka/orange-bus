2d8662b2b981d502cc345f837f7b2ae8688a3e6e
prateek-bka <prateek.bka@gmail.com>
2023-07-05 13:48:27 +0530

    initial commit frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App m-3">
+      <h3>Orange Bus Travels</h3>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
