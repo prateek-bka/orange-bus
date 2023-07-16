e3404f1e908a09eff195186f921c097b286f295b
prateek-bka <prateek.bka@gmail.com>
2023-07-16 19:55:10 +0530

    added

--- backend/routes/busesRoute.js
+++ backend/routes/busesRoute.js
@@ -70,4 +70,17 @@ router.post("/delete-bus", authMiddleware, async (req, res) => {
   }
 });
 
+router.post("/get-bus-by-id", authMiddleware, async (req, res) => {
+  try {
+    const bus = await busModel.findById(req.body._id);
+    return res.status(200).send({
+      success: true,
+      message: "Bus fetched successfully",
+      data: bus,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+});
+
 module.exports = router;
