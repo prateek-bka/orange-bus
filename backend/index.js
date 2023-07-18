1e244adef313e56d0e52b92c456d7529bf33344a
prateek-bka <prateek.bka@gmail.com>
2023-07-18 17:14:23 +0530

    added payment

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,11 @@ const port = process.env.PORT || 7500;
 
 const userRoute = require("./routes/userRoutes");
 const busesRoute = require("./routes/busesRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
 
 app.use("/api/users", userRoute);
 app.use("/api/buses", busesRoute);
+app.use("/api/bookings", bookingsRoute);
 
 app.listen(port, async () => {
   try {
